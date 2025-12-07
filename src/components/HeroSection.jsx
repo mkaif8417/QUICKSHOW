8244d24a000f23b04012ff14d334ba725cd95dde
mkaif8417 <mkaif6937@gmail.com>
2025-12-07 21:42:22 +0530

    Updating some code and Image

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,7 +11,7 @@ import { useNavigate } from 'react-router-dom';
 const HeroSection = () => {
     const navigate=useNavigate()
     return (
-        <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/backgroundImage.png")] bg-cover bg-center h-screen'>
+        <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/backgroundImagee.png")] bg-cover bg-center h-screen'>
             <img src={assets.marvelLogo} className='max-h-11 lg:h-11 mt-20 ' alt="" />
             <h1 className='text-4xl md:text-[70px] md:leading-18 font-semibold max-w-120' >Guardian <br />of The Galaxy</h1>
             <div className='flex items-center gap-4 text-gray-300'>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'  
+import { HashRouter } from 'react-router-dom'  
 import { ClerkProvider } from '@clerk/clerk-react'
-
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -12,12 +10,10 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
 }
 
-
 createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </ClerkProvider>,
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </ClerkProvider>
 )
