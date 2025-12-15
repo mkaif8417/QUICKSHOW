f3467d09f9f5af4f107f1f338da9d203222b20d3
mkaif8417 <mkaif6937@gmail.com>
2025-12-15 15:52:44 +0530

    Updating the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ border-gray-300/20 overflow-hidden transition-[width] duration-300 ${isOpen ? 'm
 <Link onClick={()=>{scrollTo(0,0) ; setIsOpen(false)}} to='/'>Home</Link>
 <Link onClick={()=>{scrollTo(0,0) ; setIsOpen(false)}} to='/movies'>Movie</Link>
 <Link onClick={()=>{scrollTo(0,0) ; setIsOpen(false)}} to='/'>Theaters</Link>
-<Link onClick={()=>{scrollTo(0,0) ; setIsOpen(false)}} to='/'>Releases</Link>
+{/* <Link onClick={()=>{scrollTo(0,0) ; setIsOpen(false)}} to='/'>Releases</Link> */}
 <Link onClick={()=>{scrollTo(0,0) ; setIsOpen(false)}} to='/favorite'>Favorites</Link>
       </div>
       <div className='flex items-center gap-8'>
